d34f41a045b4cc7bf037489d4fe1fb92c0adc4d6
Kanan33 <kanan_masuta@icloud.com>
2024-10-23 02:46:42 +0530

    Created a  Factorial program in java
    Program to calculate factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+//finding factorial of a number
+import java.util.Scanner;//importing Scanner class
+public class palidrome
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();//taking input from the user
+        int fact=1;
+        for(int i=1;i<=n;i++)
+        f=f*i;
+    }
+}
