7b08324bf0e39a2ba530f6d5d15e28e7121ea8f4
Hielke Morsink <hielke.morsink@gmail.com>
2024-10-24 11:23:58 +0200

    Optimize performance for power-of-4 check
    Using the cmath functions is far too expensive when simply bitwise operations can be used. Powers of four can be checked by two simple conditions, so I've rewritten the function to check those in a way that it's easy to follow.
    
    This allows us to remove the <cmath> header too, making compilation faster, and I've renamed the function because the word "check" is ambiguous.

--- power_of_4.cpp
+++ power_of_4.cpp
@@ -1,23 +1,32 @@
-//Given a positive number, check if it is a power of four or not .
+// Given a positive number, check if it is a power of four or not.
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 // Returns true if `n` is a power of four
-bool checkPowerOf4(unsigned n)
+constexpr bool isPowerOf4(unsigned n)
 {
-    // find `log4(n)`
-    double i = log(n) / log(4);
+    // Powers of 4 can only be > 1
+    if (n == 0)
+        return false;
 
-    // return true if `log4(n)` is an integer
-    return i == trunc(i);
+    // An integer that is the power of 4 has only one bit set. We check this by AND-ing the value with itself minus one.
+    if ((n & (n - 1)) != 0)
+        return false;
+
+    // Ensure the only set bit is in an odd position, for powers of 4 have their only set bit in positions 0, 2, 4, 6, ... etc
+    if ((n & 0b10101010101010101010101010101010) != 0)
+        return false;
+
+    // Both conditions have been met, so `n` must be a power of 4!
+    return true;
 }
 
 int main()
 {
     unsigned n = 256;
 
-    if (checkPowerOf4(n))
+    if (isPowerOf4(n))
     {
         cout << n << " is a power of 4";
     }
