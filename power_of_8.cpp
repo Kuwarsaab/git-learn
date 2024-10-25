cc09b03cda084e1bd147a413ba7d0d2fe9c7c1c5
Hielke Morsink <hielke.morsink@gmail.com>
2024-10-25 21:22:14 +0200

    Use bitwise operators and long long type
    Use bitwise operators instead of math functions to not rely on precision
    Also use long long types, to allow for much larger numbers

--- power_of_8.cpp
+++ power_of_8.cpp
@@ -1,30 +1,38 @@
-// Given a positive number, check if it is a power of 8 or not.
-
+// Given a positive number, check if it is a power of eight or not.
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-// Returns true if `n` is a power of 8
-bool checkPowerOf8(unsigned n)
+// Returns true if `n` is a power of eight
+constexpr bool isPowerOf8(unsigned long long n)
 {
-    // find `log8(n)`
-    double i = log(n) / log(8);
+    // Powers of 8 can only be > 1
+    if (n == 0)
+        return false;
 
-    // return true if `log8(n)` is an integer
-    return i - trunc(i) < 0.000001;
+    // An integer that is the power of 8 has only one bit set. We check this by AND-ing the value with itself minus one.
+    if ((n & (n - 1)) != 0)
+        return false;
+
+    // Powers of 8 have their only set bit in positions 0, 3, 6, 9, 12, ... etc (every 3rd bit, because 2^3 = 8, which is the power we're looking for!)
+    if ((n & 0b1001'0010'0100'1001'0010'0100'1001'0010'0100'1001'0010'0100'1001'0010'0100'1001ULL) == 0)
+        return false;
+
+    // Both conditions have been met, so `n` must be a power of 8!
+    return true;
 }
 
 int main()
 {
-    unsigned n = 512 * 64;
+    auto input = 144'115'188'075'855'872ULL; // A very large power of 8
 
-    if (checkPowerOf8(n))
+    if (isPowerOf8(input))
     {
-        cout << n << " is a power of 8";
+        cout << input << " is a power of 8";
     }
     else
     {
-        cout << n << " is not a power of 8";
+        cout << input << " is not a power of 8";
     }
 
     return 0;
